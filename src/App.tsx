925d17d2325fdcfae98e103a3b701c4535798da6
Ahmed gaafer <ahmeds.gaafer@gmail.com>
2024-08-30 21:25:39 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import NavBar from "./Components/Navbar";
 import { AppContext } from "./context";
 import "./styles/index.scss";
 import { MENU_OUTER_ID, MENU_PAGE_WRAP_ID } from "./constants";
 
 function App() {
-	const { context, setContext } = useContext(AppContext)!;
+	const { context } = useContext(AppContext)!;
 
 	const { ActiveComponent } = context;
-	console.log(context);
 
 	return (
 		<div id={MENU_OUTER_ID}>
